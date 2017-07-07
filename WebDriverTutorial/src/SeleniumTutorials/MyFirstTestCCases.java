d774a6177a9df31c6a10b1162f9915242c9455e5
admin <admin@HP-HOME.home>
2017-07-06 23:53:41 -0700

    yet another comment

--- WebDriverTutorial/src/SeleniumTutorials/MyFirstTestCCases.java
+++ WebDriverTutorial/src/SeleniumTutorials/MyFirstTestCCases.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class MyFirstTestCCases {
 
 	public static void main(String[] args) {
-		//some comment for the peeps
+		//some comment for the other peeps
 		System.setProperty("webdriver.gecko.driver", "C:\\Selenium\\Browsers\\geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://learn-automation.com");
